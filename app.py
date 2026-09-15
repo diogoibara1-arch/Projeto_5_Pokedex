e6db9f4f224b5a26ecbdfb24762ced3d18b4c245
Diogo.Ibara <diogoibara1@gmail.com>
2026-09-15 16:28:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import streamlit as st
-import json
+import pandas as pd
 import requests
+import json
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout='wide')
+st.title('Busca Brisa Pokemon')
 
 with open('pokemon_index.json', 'r', encoding='utf-8') as arquivo:
     nomes_pokemons = json.load(arquivo)
-
-nome = st.selectbox('Escolha um Pokemon', nomes_pokemons.values())
+## nome dos pokemons
+nome = st.selectbox("Escolha um Pikchupei: ", nomes_pokemons.values())
 
 url = f'https://pokeapi.co/api/v2/pokemon/{nome}'
 dados_pokemon = requests.get(url).json()
 
-col1, col2, col3 = st.columns(3)
+col1,col2,col3 = st.columns(3)
 
 peso = dados_pokemon['weight'] /10
 altura = dados_pokemon['height'] /10
-imc = round(peso / (altura ** 2))
+imc = round(peso / altura ** 2)
 
 with col1:
     st.image(dados_pokemon['sprites']['front_default'], width=400)
@@ -30,18 +32,15 @@ with col3:
     st.image(dados_pokemon['sprites']['front_shiny'], width=400)
     st.write('Shiny')
 
-col1, col2, col3 = st.columns(3)
+col1,col2,col3 = st.columns(3)
 with col1:
-    st.metric('Altura', f'{altura} M')
-
+    st.metric("A Altura é", f"{altura} M")
 with col2:
-    st.metric('IMC', imc)
-
+    st.metric("O peso é", f"{peso} KG")
 with col3:
-    st.metric('Peso', f'{peso} KG')
+    st.metric("O IMC é", f"{imc}")
 
-
-tipos, status, locais, habilidades = st.tabs(['Tipos', 'Status', 'Locais', 'Habilidades'])
+tipos, status, locais, habilidades = st.tabs (["tipos", "status", "locais", "habilidades"])
 
 with tipos:
     for i in dados_pokemon['types']:
